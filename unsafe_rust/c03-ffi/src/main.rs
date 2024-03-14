e68e47d7bcc70a38a196f2dbd844e3832ba583ce
Branislav Hozza <brankohozza@gmail.com>
2024-03-14 13:22:52 +0100

    :sparkles: unsafe ffi

--- unsafe_rust/c03-ffi/src/main.rs
+++ unsafe_rust/c03-ffi/src/main.rs
@@ -1,3 +1,14 @@
+use std::ffi::CString;
+
+extern crate libc;
+
+extern "C" {
+    fn puts(s: *const libc::c_char) -> libc::c_int;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let safe_hello_world = CString::new("Hello, world!").expect("CString::new failed");
+    unsafe {
+        puts(safe_hello_world.as_ptr());
+    }
 }
